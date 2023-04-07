4794be1a15c34a783b7ab90203b5016caa397fec
Bharath <rk.bharathram123@gmail.com>
2023-04-07 18:38:04 +0530

    some changes were done

--- lib/login/app_state.dart
+++ lib/login/app_state.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lunch_app/home/home_page.dart';
 import 'package:lunch_app/login/login_page.dart';
 
+import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LunchApp extends StatefulWidget {
@@ -18,32 +19,24 @@ class _LunchAppState extends State<LunchApp> {
     return MaterialApp(
       title: 'New project',
       theme: ThemeData(
-       primarySwatch:Colors.deepPurple,
-       
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 23,
-            wordSpacing: 3,
-            letterSpacing: 1.2,
-            color: Color.fromARGB(221, 7, 6, 6),
-            fontWeight: FontWeight.bold,
-            
+          primarySwatch: Colors.deepPurple,
+          textTheme: TextTheme(
+            displayLarge: GoogleFonts.greatVibes(
+              fontSize: 21,
+              wordSpacing: 3,
+              letterSpacing: 1.5,
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          
-        ),
-        // elevatedButtonTheme: ElevatedButtonThemeData(
-        //   style: ButtonStyle(
-        //     padding: MaterialStatePropertyAll(
-        //       EdgeInsets.only(
-        //         bottom: 10,
-        //         left: 10,
-        //         right: 10,
-        //         top: 10,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-      ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  padding: MaterialStatePropertyAll(EdgeInsets.only(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            top: 0,
+          ))))),
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
