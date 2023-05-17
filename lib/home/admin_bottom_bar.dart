33eb0beb39c6675a5d9e2a429d61cf4316e78f67
maheshEasy <maheshgonela9676@gmail.com>
2023-05-17 19:39:00 +0530

    search bar and share button added

--- lib/home/admin_bottom_bar.dart
+++ lib/home/admin_bottom_bar.dart
@@ -1,29 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'package:lunch_app/home/home_page_buttons.dart';
 
-Row adminbottombar(BuildContext context, totallunchcount, totaleggcount, meal_quantity,{VoidCallback? onPress}) {
+Row adminbottombar(
+    BuildContext context, totallunchcount, totaleggcount, meal_quantity,
+    {VoidCallback? onPress}) {
   return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.start,
     children: [
       // we used "false , false" for balanceing buttons like copy and yes btns
-      logoutsubmitSharebtn(
-          1,
-          context,
-          false,
-          false,
-          totallunchcount.toString(),
-          totaleggcount.toString(),
-          meal_quantity
-         ),
-         SizedBox(width: 20,),
-     Text(
-        'and paste in whatsapp',
-        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-              fontSize: 21,
-             color: Color.fromARGB(255, 7, 1, 0),
-            ),
-      ),
+      logoutsubmitSharebtn(1, context, false, false, totallunchcount.toString(),
+          totaleggcount.toString(), meal_quantity),
+     
     ],
   );
 }
--- lib/home/home_page_buttons.dart
+++ lib/home/home_page_buttons.dart
@@ -2,10 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:toast/toast.dart';
 
-List buttonNames = ['yes','Copy'];
+List buttonNames = ['yes','share'];
 CollectionReference lunch = FirebaseFirestore.instance
     .collection('lunch_${now.day}-${now.month}-${now.year}');
 var now = DateTime.now();
@@ -18,71 +18,73 @@ Widget logoutsubmitSharebtn(
     totallunchcount,
     totaleggcount,
     meal_quantity,
-    {VoidCallback? onPress}
-   ) {
+    {VoidCallback? onPress}) {
   if (buttonNamesIndex == 1) {
     return ElevatedButton.icon(
-        onPressed: () {
-          Toast.show("Copied!",
-              duration: Toast.lengthShort, gravity: Toast.bottom);
-          final whatsapptext = ClipboardData(
-              // -- for -- egg -- Egg : ${int.parse(totaleggcount) * egg_price} Rs
-              text: "Lunch : ${meal_quantity} ");
-          Clipboard.setData(whatsapptext);
-        },
-        style: ButtonStyle(
-          textStyle: MaterialStatePropertyAll(GoogleFonts.lato(
+      onPressed: ()async {
+       await Share.share("Lunch : ${meal_quantity}");
+      },
+      style: ButtonStyle(
+        textStyle: MaterialStatePropertyAll(
+          GoogleFonts.lato(
             fontSize: 17,
-        
             color: Colors.black,
             fontWeight: FontWeight.bold,
-          )),
-          elevation: MaterialStatePropertyAll(4),
-          backgroundColor: MaterialStatePropertyAll(
-            Color.fromARGB(255, 200, 192, 234),
-          ),
-          padding: MaterialStatePropertyAll(EdgeInsets.all(4)),
-          minimumSize: MaterialStatePropertyAll(
-            Size(40, 40),
           ),
         ),
-        icon: Icon(Icons.copy, color: Colors.black87),
-        label: Text(
-          buttonNames[buttonNamesIndex],
-          style: TextStyle(
-            color: Colors.black87, // text color
-          ),
-        ));
-  } else{
+        elevation: MaterialStatePropertyAll(4),
+        backgroundColor: MaterialStatePropertyAll(
+          Color.fromARGB(255, 200, 192, 234),
+        ),
+        padding: MaterialStatePropertyAll(EdgeInsets.all(4)),
+        minimumSize: MaterialStatePropertyAll(
+          Size(40, 40),
+        ),
+      ),
+      icon: Icon(Icons.share, color: Colors.black87),
+      label: Text(
+        buttonNames[buttonNamesIndex],
+        style: TextStyle(
+          color: Colors.black87, // text color
+        ),
+      ),
+    );
+
+  }
+  
+   else {
     return ElevatedButton(
-        style: ButtonStyle(
-            textStyle: MaterialStatePropertyAll(GoogleFonts.lato(
+      style: ButtonStyle(
+          textStyle: MaterialStatePropertyAll(
+            GoogleFonts.lato(
               fontSize: 23,
               color: Colors.black,
               fontWeight: FontWeight.bold,
-            )),
-            elevation: MaterialStatePropertyAll(4),
-            backgroundColor:
-                MaterialStatePropertyAll(Color.fromARGB(255, 200, 192, 234)),
-            padding: MaterialStatePropertyAll(EdgeInsets.all(4)),
-            minimumSize: MaterialStatePropertyAll(Size(20, 20))),
-        onPressed: () async {
-          await callbackforyes(
-              buttonNamesIndex, context, _lunchisChecked, _eggisChecked);
-          // Handle the result here
-          onPress?.call();
-        },
-        child: Text(
-          buttonNames[buttonNamesIndex],
-          style: TextStyle(
-            color: Colors.black87, // text color
+            ),
           ),
-        ));
+          elevation: MaterialStatePropertyAll(4),
+          backgroundColor:
+              MaterialStatePropertyAll(Color.fromARGB(255, 200, 192, 234)),
+          padding: MaterialStatePropertyAll(EdgeInsets.all(4)),
+          minimumSize: MaterialStatePropertyAll(Size(20, 20))),
+      onPressed: () async {
+        await callbackforyes(
+            buttonNamesIndex, context, _lunchisChecked, _eggisChecked);
+        // Handle the result here
+        onPress?.call();
+      },
+      child: Text(
+        buttonNames[buttonNamesIndex],
+        style: TextStyle(
+          color: Colors.black87, // text color
+        ),
+      ),
+    );
   }
 }
 
-Future<void> callbackforyes(int buttonNamesIndex,
-    BuildContext context, _lunchisChecked, _eggisChecked) async {
+Future<void> callbackforyes(int buttonNamesIndex, BuildContext context,
+    _lunchisChecked, _eggisChecked) async {
   if (buttonNamesIndex == 0) {
     Toast.show("Successfully Updated!",
         duration: Toast.lengthShort, gravity: Toast.bottom);
